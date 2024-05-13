cebb8480b742dce7d757f6814c57ea1d7d314fa3
AIhelp-guru <148708340+AIhelp-guru@users.noreply.github.com>
2024-05-13 08:01:35 -0400

    1

--- create.js
+++ create.js
@@ -66,7 +66,7 @@ function renderPage(num) {
             var textStr = strings.join(" "); // Join all text strings into a single string with spaces
             pageText = (textStr); // Store the readable text
             // alert(pageText);
-            $("#pt").html(pageText);
+            $("#pt").append(pageText);
 
         });
     });
