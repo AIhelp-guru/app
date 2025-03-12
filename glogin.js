1c7be67eb2d7627652edc19bf6c6b4d7ec8b1157
AIhelp-guru <148708340+AIhelp-guru@users.noreply.github.com>
2025-03-11 22:27:44 -0400

    Update glogin.js

--- glogin.js
+++ glogin.js
@@ -48,7 +48,7 @@ function login(email, token){
     		sessionStorage.setItem('user', email);
             sessionStorage.setItem('token', token);
             sessionStorage.setItem('jobs', data);
-            alert(data);
+            //alert(data);
             location.href = "bluebook.html";
             
             })
